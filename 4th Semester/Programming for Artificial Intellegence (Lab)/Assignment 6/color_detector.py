1338a9d039af4212efaa7d7d1a5c26d839a6f7cf
Ali Maqsood <152074599+PythonMindset@users.noreply.github.com>
2025-10-17 10:42:01 +0500

    Add files via upload

--- 4th Semester/Programming for Artificial Intellegence (Lab)/Assignment 6/color_detector.py
+++ 4th Semester/Programming for Artificial Intellegence (Lab)/Assignment 6/color_detector.py
@@ -1,21 +1,17 @@
-from PIL import Image
+import cv2
 from sklearn.cluster import KMeans
 import numpy as np
 
 def extract(image_path, k=5):
-    image=Image.open(image_path)
-    image=image.resize((150, 150))
-    pixels=np.array(image).reshape(-1, 3)
-
+    image=cv2.imread(image_path)
+    image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image=cv2.resize(image, (150, 150))
+    pixels=image.reshape(-1, 3)
     kmeans=KMeans(n_clusters=k, n_init=10)
     kmeans.fit(pixels)
-
     colors=kmeans.cluster_centers_.astype(int)
     hex_colors=[]
-    for color in colors:
-        r,g,b=color
+    for r,g,b in colors:
         hex_code=f"#{r:02x}{g:02x}{b:02x}"
         hex_colors.append(hex_code)
-
     return hex_colors
-
